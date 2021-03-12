73cfad9bbb3af27c169f9df24743d6c5920f816a
RedYetiDev <38299977+RedYetiDev@users.noreply.github.com>
2021-03-12 17:58:29 -0500

    Github page

--- tools.js
+++
@@ -1,8 +0,0 @@
-const image = require("./tools/image")
-const video = require("./tools/video")
-const loader = require("./tools/loader")
-const server = require("./tools/server")
-module.exports.image = image
-module.exports.loader = loader
-module.exports.video = video
-module.exports.server = server
